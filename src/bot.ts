c86d2f6561ddfaf5cfbf9fc9bb2782a2890c470b
Andrew <merrillman32@gmail.com>
2020-10-07 09:59:37 -0600

    moved deletemessages to its own file

--- src/bot.ts
+++ src/bot.ts
@@ -2,7 +2,6 @@ const { Client, Permissions, User, WebhookClient } = require("discord.js");
 import { Collection, Message } from "discord.js";
 import fs from "fs";
 
-// var { token, webhook, prefix } = require('./config.json');
 import { token, webhook, prefix } from "./config.json";
 
 const bot = new Client();
@@ -36,52 +35,3 @@ bot.on("message", (message: Message) => {
 });
 
 bot.login(token);
-
-// // -------------------- Commands/Events handling --------------------
-
-// bot.on("message", async (message: typeof Message) => {
-//   if (!message.content.startsWith(prefix) || message.author.bot) return;
-//   const args = message.content.slice(prefix.length).trim().split(/ +/);
-//   const command = args.shift().toLowerCase();
-//   if (command === "deletemessages") {
-//     if (message.member.hasPermission(Permissions.FLAGS.MANAGE_MESSAGES)) {
-//       try {
-//         const messages = await message.channel.fetchMessages();
-//         await message.channel.bulkDelete(messages).catch(() => {
-//           message.author.send("Error deleting messages");
-//         });
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     } else {
-//       message.author.send("You do not have permission to run this command");
-//     }
-//   } else if (command === "avatar") {
-//     if (!message.mentions.users.size) {
-//       return message.channel.send(
-//         `Your avatar: <${message.author.displayAvatarURL({
-//           format: "png",
-//           dynamic: true,
-//         })}>`
-//       );
-//     }
-//     const avatarList = message.mentions.users.map((user: typeof User) => {
-//       return `${user.username}'s avatar: <${user.displayAvatarURL({
-//         format: "png",
-//         dynamic: true,
-//       })}>`;
-//     });
-//     message.delete(1000);
-//     message.author.send(avatarList);
-//   }
-// });
-
-// // const cmds = ['aliases', 'commands'];
-// // const handlers = ['command', 'event'];
-
-// // cmds.forEach((x) => (bot[x] = new Collection()));
-// // handlers.forEach((x) => require(`./handlers/${x}`)(bot, logsWebhook));
-
-// // -------------------- Login --------------------
-
-// bot.login(token);
