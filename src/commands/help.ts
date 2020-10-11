8dbb0f84d7c910c21e554314869e2362dd357cd5
Andrew <merrillman32@gmail.com>
2020-10-10 18:19:30 -0600

    added help command that parses each command to print out information (#9)

---
+++ src/commands/help.ts
@@ -0,0 +1,26 @@
+import { Message } from "discord.js";
+import { prefix } from "../config.json";
+import fs from "fs";
+
+module.exports = {
+  name: "help",
+  usage: `${prefix}help`,
+  description: "Shows all of the bot commands available to the user",
+  execute(message: Message, args: string[]) {
+    let help = "Here are the available commands!\n";
+    const commandFiles = fs
+      .readdirSync(`${__dirname}`)
+      .filter((file) => file.endsWith(".js"));
+    for (const file of commandFiles) {
+      const command = require(`${__dirname}/${file}`);
+      if (
+        (command.permission &&
+          message.member.hasPermission(command.permission)) ||
+        !command.permission
+      ) {
+        help += `**${command.usage}:** ${command.description}\n`;
+      }
+    }
+    message.channel.send(help);
+  },
+};
--- src/commands/server-info.ts
+++ src/commands/server-info.ts
@@ -1,7 +1,9 @@
 import { Message } from "discord.js";
+import { prefix } from "../config.json";
 
 module.exports = {
   name: "server-info",
+  usage: `${prefix}server-info`,
   description: "Get basic server info",
   execute(message: Message, args: string[]) {
     message.channel.send(`Server name: ${message.guild.name}\
