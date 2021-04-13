01c8526b7ad5955bdff39ebc3a77547c36b45d3a
amerrill <merrillman32@gmail.com>
2021-04-12 20:39:25 -0600

    Command: random-clip command is working

--- src/commands/Channels/random-clip.ts
+++ src/commands/Channels/random-clip.ts
@@ -1,4 +1,4 @@
-import { Message} from "discord.js";
+import { Collection, Message, TextChannel } from "discord.js";
 import { prefix } from "../../config.json";
 import Utils from "../../util/utils";
 
@@ -7,14 +7,21 @@ module.exports = {
   usage: `${prefix}random-clip`,
   description: "Get a DM with a random Twitch clip of tennisshoesfan",
   execute(message: Message, args: string[]) {
-    //const channel = message.client.channels.get("762863369400221696");
-    //console.log(channel.messages)
-    //channel.messages.fetch({ limit: 100  }).then((messages: Message[]) => {
-    //  console.log(`Received ${messages.length} messages`);
-    //  messages.forEach((message: Message) => {
-    //    console.log(message.content)
-    //  })
-    //})
-    //Utils.dm_response(message, response);
+    const channel = message.client.channels.cache.get("762863369400221696")
+    if (channel) {
+        const messages = (channel as TextChannel).messages
+        messages.fetch({ limit: 100 }).then((messages: Collection<string, Message>) => {
+          messages.forEach((message: Message) => {
+            if (message.content.includes('https://clips.twitch.tv/')) {
+              console.log(message.content)
+            }
+          })
+          const twitch_clips = messages
+                                .filter(message => message.content.includes('https://clips.twitch.tv/'))
+                                .map(message => message.content)
+          const response: string = twitch_clips[Math.floor(Math.random() * twitch_clips.length) as number]
+          Utils.dm_response(message, response)
+        })
+    }
   },
 };
