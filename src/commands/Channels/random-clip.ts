f281b87dfdc27fdb1ee41c5ee6c67b13d22f3051
Andrew <merrillman32@gmail.com>
2021-04-14 19:54:05 -0600

    moved twitch channel id to config (#14)

--- src/commands/Channels/random-clip.ts
+++ src/commands/Channels/random-clip.ts
@@ -1,5 +1,5 @@
 import { Collection, Message, TextChannel } from "discord.js";
-import { prefix } from "../../config.json";
+import { prefix, twitch_clip_channel_id } from "../../config.json";
 import Utils from "../../util/utils";
 
 module.exports = {
@@ -7,21 +7,21 @@ module.exports = {
   usage: `${prefix}random-clip`,
   description: "Get a DM with a random Twitch clip of tennisshoesfan",
   execute(message: Message, args: string[]) {
-    const channel = message.client.channels.cache.get("762863369400221696")
-    if (channel) {
-        const messages = (channel as TextChannel).messages
-        messages.fetch({ limit: 100 }).then((messages: Collection<string, Message>) => {
-          messages.forEach((message: Message) => {
-            if (message.content.includes('https://clips.twitch.tv/')) {
-              console.log(message.content)
-            }
+    if (twitch_clip_channel_id) {
+      const channel = message.client.channels.cache.get(twitch_clip_channel_id)
+      if (channel) {
+          const messages = (channel as TextChannel).messages
+          messages.fetch({ limit: 100 }).then((messages: Collection<string, Message>) => {
+            const twitch_clips = messages
+                                  .filter(message => message.content.includes('https://clips.twitch.tv/'))
+                                  .map(message => message.content)
+            const response: string = twitch_clips[Math.floor(Math.random() * twitch_clips.length) as number]
+            Utils.dm_response(message, response)
           })
-          const twitch_clips = messages
-                                .filter(message => message.content.includes('https://clips.twitch.tv/'))
-                                .map(message => message.content)
-          const response: string = twitch_clips[Math.floor(Math.random() * twitch_clips.length) as number]
-          Utils.dm_response(message, response)
-        })
+      }
+    } else {
+      const response: string = "Command Disabled: Twitch channel ID missing in config."
+      Utils.dm_response(message, response)
     }
   },
 };
